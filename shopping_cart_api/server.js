ccf67f2d28928fcf4614137b97861ac61b610074
haywhyoh <adefeyisayo998@gmail.com>
2023-04-11 12:18:46 +0100

    created the express erver

--- shopping_cart_api/server.js
+++ shopping_cart_api/server.js
@@ -1,6 +1,15 @@
 const express = require('express');
-
+const router = require('./routes/index');
 const app = express();
+const db = require('./services/db');
+
+app.use(express.json());
+app.use('/api', router);
+
+app.get('/', (req, res) => {
+  res.send('I dey here');
+});
+db.isAlive();
 
 app.listen(3000, () => {
   console.log('App running at port 3000');
