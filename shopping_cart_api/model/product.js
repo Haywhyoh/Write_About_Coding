f9bff03209b72ddb8eebf9644f43df3413dda9c6
haywhyoh <adefeyisayo998@gmail.com>
2023-04-11 12:19:35 +0100

    the User model

--- shopping_cart_api/model/product.js
+++ shopping_cart_api/model/product.js
@@ -3,12 +3,10 @@ const Mongoose = require('mongoose');
 const Schema = Mongoose.Schema;
 
 const cartSchema = new Schema({
-  item: [{
-    item: { type: String, required: true },
-    price: { type: Number, required: true },
-    quantity: { type: Number, default: 1 }
-  }],
-  total: { type: Number, required: true }
+  item: { type: String, required: true },
+  price: { type: Number, required: true },
+  quantity: { type: Number, default: 1 },
+  total: { type: Number, default: 0 }
 }, { timestamps: true });
 
 module.exports = Mongoose.model('Cart', cartSchema);
