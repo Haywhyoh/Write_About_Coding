aa4fcdef7e1179db58fe68eed113eb4dc2c9fb9b
haywhyoh <adefeyisayo998@gmail.com>
2023-04-12 01:12:58 +0100

    added the login and register route

--- shopping_cart_api/routes/index.js
+++ shopping_cart_api/routes/index.js
@@ -1,11 +1,15 @@
 const express = require('express');
 const router = express.Router();
 const { addNewCart, retrieveItems, getItem, deleteItem, updateItem } = require('../controllers/CartController');
+const { login, register } = require('../auth/auth');
+const { userAuth } = require('../middlewares/auth');
 
+router.route('/register').post(register);
+router.route('/login').post(login);
 router.route('/cart/:id').get(getItem);
 router.route('/cart').get(retrieveItems);
-router.route('/cart').post(addNewCart);
-router.route('/cart/:id').put(updateItem);
-router.route('/cart/:id').delete(deleteItem);
+router.route('/cart').post(userAuth, addNewCart);
+router.route('/cart/:id').put(userAuth, updateItem);
+router.route('/cart/:id').delete(userAuth, deleteItem);
 
 module.exports = router;
