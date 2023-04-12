00b84f4df179db8d980ec51edb132e49126a8f51
haywhyoh <adefeyisayo998@gmail.com>
2023-04-12 01:12:19 +0100

    created a one to many relationship for the cart

--- shopping_cart_api/model/product.js
+++ shopping_cart_api/model/product.js
@@ -3,10 +3,16 @@ const Mongoose = require('mongoose');
 const Schema = Mongoose.Schema;
 
 const cartSchema = new Schema({
-  item: { type: String, required: true },
-  price: { type: Number, required: true },
-  quantity: { type: Number, default: 1 },
-  total: { type: Number, default: 0 }
+  items: [{
+    item: { type: String, required: true },
+    price: { type: Number, required: true },
+    quantity: { type: Number, default: 1 }
+  }],
+  total: { type: Number, default: 0 },
+  user: {
+    type: String,
+    ref: 'user'
+  }
 }, { timestamps: true });
 
 module.exports = Mongoose.model('Cart', cartSchema);
