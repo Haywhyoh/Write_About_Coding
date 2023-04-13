db0e51d6cfa9784088a1d40def80968f057e1e09
haywhyoh <adefeyisayo998@gmail.com>
2023-04-13 14:09:26 +0100

    added the admin auth

--- shopping_cart_api/middlewares/auth.js
+++ shopping_cart_api/middlewares/auth.js
@@ -11,6 +11,28 @@ exports.userAuth = async (req, res, next) => {
         if (decodedData.role !== 'user') {
           return res.status(401).json({ message: 'You are not a user' });
         } else {
+          req.username = decodedData.username;
+          next();
+        }
+      }
+    });
+  } else {
+    return res.status(401).json({ message: 'Not Authorized as token not found' });
+  }
+}
+;
+
+exports.adminAuth = async (req, res, next) => {
+  const token = req.cookies.jwt;
+  if (token) {
+    jwt.verify(token, secretKey, (err, decodedData) => {
+      if (err) {
+        return res.status(401).json({ message: 'Not Authorized' });
+      } else {
+        if (decodedData.role !== 'admin') {
+          return res.status(401).json({ message: 'You are not an admin' });
+        } else {
+          req.username = decodedData.username;
           next();
         }
       }
